d9aa1c006ec4c3fcf80dba9b463841a444dc98d2
Haydara <haydaraal2005@gmail.com>
2022-07-03 15:29:24 -0700

    Add project files.

---
+++ MathH/MathH.cs
@@ -0,0 +1,42 @@
+﻿namespace MathH;
+
+
+/// <summary>
+///     Provides a list of most used mathematics constants.
+/// </summary>
+public static class Consts
+{
+    /// <summary>
+    ///     Represents the natural logarithmic base, specified by the constant, e.
+    /// </summary>
+    public const double E = 2.7182818284590451;
+
+    /// <summary>
+    ///     Represents the ratio of the circumference of a circle to its diameter, specified
+    ///     by the constant, π.
+    /// </summary>  
+    public const double PI = 3.1415926535897931;
+
+    /// <summary>
+    ///     Represents the number of radians in one turn, specified by the constant, τ.
+    /// </summary>
+    public const double Tau = 6.2831853071795862;
+
+    ///<summary>
+    ///     Represents the Euler-Mascheroni constant.
+    ///</summary>
+    public const double Gamma = 0.57721566490153286060;
+
+    ///<summary>
+    ///     Represents the golden ratio, specified by the constant φ.
+    /// </summary>
+    public const double Phi = 1.61803398874989484820;
+}
+
+
+public static class PreCalculus
+{
+      
+}
+
+
---
+++ ShowCase/Program.cs
@@ -0,0 +1,34 @@
+﻿using MathH;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            
+        } while (true);
+        return;
+        
+        
+
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, input data and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
