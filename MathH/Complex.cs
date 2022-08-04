db07561eb5ce3df1925518e2e3cbb3e68ea8cbe0
Haydara <haydaraal2005@gmail.com>
2022-08-04 14:28:48 -0700

    Overriden ToString, Equals, and GetHashCode functions. Added operator functions.

--- MathH/Complex.cs
+++ MathH/Complex.cs
@@ -34,8 +34,8 @@ namespace cnums
                 Real = Re;
                 Immaginary = Im;
             }
-            Real = R * Maths.Cos(Theta);
-            Immaginary = R * Maths.Sin(Theta);
+            Real = R * Cos(Theta);
+            Immaginary = R * Sin(Theta);
         }
 
         public double Modulus()
@@ -43,23 +43,98 @@ namespace cnums
 
         public double Arg()
         {
-            double t, r;
-            r = Sqrt(Power(Real) + Power(Immaginary));
+            double magnitude = Abs(new Complex(Real, Immaginary));
 
-            double cs, sn;
+            double snTheta = Asin(Immaginary / magnitude);
+            double csTheta = Acos(Real / magnitude);
 
-            cs = Real / r;
-            sn = Immaginary / r;
+            double theta = (Abs(snTheta + csTheta)) / 2;
 
-            t = Acos(cs);
-
-            if (cs >= 0 && sn >= 0) return t;
-            else if (cs <= 0 && sn >= 0) return PI - t;
-            else if (cs <= 0 && sn <= 0) return PI + t;
-            else return -t;
-            
+            if (snTheta >= 0 && csTheta >= 0) return theta;
+            else if (snTheta >= 0 && csTheta <= 0) return PI - theta;
+            else if (snTheta <= 0 && csTheta <= 0) return PI + theta;
+            else return -theta;
         }
 
+        public Complex Conjugate()
+            => new(Real, -Immaginary);
 
+        public string DescartianForm()
+        {
+            string s = Real.ToString();
+
+            if (Immaginary > 0) s += $"+{Immaginary}i";
+            else if( Immaginary == 0) { }
+            else s += $"{Immaginary}i";
+
+            return s;
+        }
+
+        public override string ToString()
+            => DescartianForm();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != this.GetType()) return false;
+            Complex complex = (Complex)obj;
+            return (this.Im == complex.Im && this.Re == complex.Re);
+        }
+
+        public bool isConjugate(Complex c1)
+            => (this.Re == c1.Re && this.Im == -c1.Im);
+
+        #region Plus operator.
+        public static Complex operator +(Complex c1)
+            => c1;
+        public static Complex operator +(Complex c1, Complex c2)
+            => new(c1.Re + c2.Re, c1.Im + c2.Im);
+        public static Complex operator +(Complex c1, int n)
+            => new(c1.Re + n, c1.Im);
+        public static Complex operator +(Complex c1, long n)
+            => new(c1.Re + n, c1.Im);
+        public static Complex operator +(Complex c1, double n)
+            => new(c1.Re + n, c1.Im);
+        public static Complex operator +(Complex c1, short n)
+            => new(c1.Re + n, c1.Im);
+        public static Complex operator +(Complex c1, float n)
+            => new(c1.Re + n, c1.Im);
+        #endregion
+
+
+        #region Minus operator.
+        public static Complex operator -(Complex c1)
+            => new(-c1.Re, -c1.Im);
+        public static Complex operator -(Complex c1, Complex c2)
+            => new(c1.Re - c2.Re, c1.Im - c2.Im);
+        public static Complex operator -(Complex c1, int n)
+            => new(c1.Re - n, c1.Im - n);
+        public static Complex operator -(Complex c1, short n)
+            => new(c1.Re - n, c1.Im - n);
+        public static Complex operator -(Complex c1, long n)
+            => new(c1.Re - n, c1.Im - n);
+        public static Complex operator -(Complex c1, double n)
+            => new(c1.Re - n, c1.Im - n);
+        public static Complex operator -(Complex c1, float n)
+            => new(c1.Re - n, c1.Im - n);
+        public static Complex operator -(Complex c1, uint n)
+            => new(c1.Re - n, c1.Im - n);
+        public static Complex operator -(Complex c1, ushort n)
+            => new(c1.Re - n, c1.Im - n);
+        public static Complex operator -(Complex c1, ulong n)
+            => new(c1.Re - n, c1.Im - n);
+        #endregion
+
+
+        public static Complex operator *(Complex c1, Complex c2)
+            => new(c1.Re * c2.Re - c1.Im * c2.Im, c2.Im * c1.Re + c1.Im * c2.Re);
+
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            c1 *= c2.Conjugate();
+            c1.Re /= c2.Modulus();
+            c1.Im /= c2.Modulus();
+            return c1;
+        }
     }
 }
