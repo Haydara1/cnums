2e7f5e03b31c91b9504407538310aa35e1b6fba2
Haydara <haydaraal2005@gmail.com>
2022-08-18 13:49:51 -0700

    Added operators between symbols and doubles. Added coefficient and power property.

--- MathH/Symbolic coding classes/Symbol.cs
+++ MathH/Symbolic coding classes/Symbol.cs
@@ -11,7 +11,33 @@
             private set { Sym = value; }
         }
 
-        public Symbol(string symbol) => sym = symbol;
+        private double Coefficient = 1;
+
+        public double coefficient
+        {
+            get { return Coefficient; }
+            private set { Coefficient = value; }
+        }
+
+        private double Power = 1;
+
+        public double power
+        {
+            get { return Power; }
+            private set { Power = value; }  
+        }
+
+        public Symbol(string symbol)
+        {
+            this.Sym = symbol;
+        }
+
+        public Symbol(string symbol, double coefficient = 1, double power = 1)
+        {
+            this.Sym = symbol;
+            this.Coefficient = coefficient;
+            this.Power = power;
+        }
 
         public override bool Equals(object? obj)
         {
@@ -31,7 +57,14 @@
             => sym;
 
 
+        public static Symbol operator +(Symbol sym) => sym;
+        public static Symbol operator -(Symbol sym) => new(sym.Sym, -sym.coefficient);
 
+        public static Symbol operator *(Symbol sym, double num) => new(sym.Sym, sym.coefficient * num);
+        public static Symbol operator /(Symbol sym, double num) => new(sym.Sym, sym.coefficient / num);
+
+        public static Symbol operator *(double num, Symbol sym) => new(sym.Sym, sym.coefficient * num);
+        public static Symbol operator /(double num, Symbol sym) => new(sym.Sym, num / sym.coefficient, -sym.power);
     }
 
 
