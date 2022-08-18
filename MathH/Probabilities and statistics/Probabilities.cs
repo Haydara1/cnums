9230f070359a8f0f2bcb1eba74c742456541a32b
Haydara <haydaraal2005@gmail.com>
2022-08-18 13:42:29 -0700

    Added distinct classes for probabilities and statistics functions.

---
+++ MathH/Probabilities and statistics/Probabilities.cs
@@ -0,0 +1,54 @@
+﻿namespace cnums;
+
+public static class Probabilites
+{
+    #region nPr
+
+#pragma warning disable IDE1006 // Naming Styles
+    public static double nPr(double n, double r)
+#pragma warning restore IDE1006 // Naming Styles
+        => Maths.Factorial(n) / Maths.Factorial(n - r);
+
+#pragma warning disable IDE1006 // Naming Styles
+    public static float nPr(float n, float r)
+        => Maths.Factorial((float)n) / Maths.Factorial((float)n - r);
+
+    public static int nPr(int n, int r)
+        => Maths.Factorial((int)n) / Maths.Factorial((int)n - r);
+
+    public static long nPr(long n, long r)
+        => Maths.Factorial((long)n) / Maths.Factorial((long)n - r);
+
+    public static decimal nPr(decimal n, decimal r)
+        => Maths.Factorial((decimal)n) / Maths.Factorial((decimal)n - r);
+
+    public static short nPr(short n, short r)
+        => (short)(Maths.Factorial((short)n) / Maths.Factorial((short)n - r));
+
+
+    #endregion
+
+    #region nCr
+
+    public static double nCr(double n, double r)
+        => Maths.Factorial((double)n) / (Maths.Factorial((double)n - r) * Maths.Factorial((double)r));
+
+    public static float nCr(float n, float r)
+        => (float)(Maths.Factorial((float)n) / (Maths.Factorial((float)n - r) * Maths.Factorial((float)r)));
+
+    public static int nCr(int n, int r)
+        => (int)Maths.Factorial((int)n) / (int)(Maths.Factorial((int)n - r) * Maths.Factorial((int)r));
+
+    public static long nCr(long n, long r)
+        => (long)Maths.Factorial((long)n) / (long)(Maths.Factorial((long)n - r) * Maths.Factorial((long)r));
+
+    public static short nCr(short n, short r)
+        => (short)((short)Maths.Factorial((short)n) / (short)(Maths.Factorial((short)n - r) * Maths.Factorial((short)r)));
+
+    public static decimal nCr(decimal n, decimal r)
+        => (decimal)Maths.Factorial((decimal)n) / (decimal)(Maths.Factorial((decimal)(n - r)) * Maths.Factorial((decimal)r));
+
+#pragma warning restore IDE1006 // Naming Styles
+    #endregion
+}
+
---
+++ MathH/Probabilities and statistics/Statistics.cs
@@ -0,0 +1,163 @@
+﻿namespace cnums;
+
+public static class Statistics
+{
+    #region Max and min functions.
+
+    public static int Max(int a, int b)
+    {
+        if (a >= b) return a;
+        return b;
+    }
+
+    public static double Max(double a, double b)
+    {
+        if (a >= b) return a;
+        return b;
+    }
+
+    public static float Max(float a, float b)
+    {
+        if (a >= b) return a;
+        return b;
+    }
+
+    public static long Max(long a, long b)
+    {
+        if (a >= b) return a;
+        return b;
+    }
+
+    public static short Max(short a, short b)
+    {
+        if (a >= b) return a;
+        return b;
+    }
+
+    public static decimal Max(decimal a, decimal b)
+    {
+        if (a >= b) return a;
+        return b;
+    }
+
+    public static int Min(int a, int b)
+    {
+        if (a <= b) return a;
+        return b;
+    }
+
+    public static double Min(double a, double b)
+    {
+        if (a <= b) return a;
+        return b;
+    }
+
+    public static float Min(float a, float b)
+    {
+        if (a <= b) return a;
+        return b;
+    }
+
+    public static long Min(long a, long b)
+    {
+        if (a <= b) return a;
+        return b;
+    }
+
+    public static short Min(short a, short b)
+    {
+        if (a <= b) return a;
+        return b;
+    }
+
+    public static decimal Min(decimal a, decimal b)
+    {
+        if (a <= b) return a;
+        return b;
+    }
+
+    #endregion
+
+    #region Mean functions
+
+    public static double Mean(double[] array)
+    {
+        double sum = 0;
+
+        foreach (double d in array) sum += d;
+
+        return sum / array.Length;
+    }
+
+    public static float Mean(float[] array)
+    {
+        float sum = 0;
+
+        foreach (float f in array) sum += f;
+
+        return sum / array.Length;
+    }
+
+    public static int Mean(int[] array)
+    {
+        int sum = 0;
+
+        foreach (int i in array) sum += i;
+
+        return sum / array.Length;
+    }
+
+    public static long Mean(long[] array)
+    {
+        long sum = 0;
+
+        foreach (long l in array) sum += l;
+
+        return sum / array.Length;
+    }
+
+    public static short Mean(short[] array)
+    {
+        short sum = 0;
+
+        foreach (short s in array) sum += s;
+
+        return (short)(sum / array.Length);
+    }
+
+    public static decimal Mean(decimal[] array)
+    {
+        decimal sum = 0;
+
+        foreach (decimal d in array) sum += d;
+
+        return sum / array.Length;
+    }
+
+    public static uint Mean(uint[] array)
+    {
+        uint sum = 0;
+
+        foreach (uint u in array) sum += u;
+
+        return (uint)(sum / array.Length);
+    }
+
+    #endregion
+
+    #region Median functions.
+
+    public static double Median(double[] array)
+    {
+        Array.Sort(array);
+
+        int index;
+
+        if (array.Length % 2 != 0) index = (array.Length + 1) / 2;
+        else index = array.Length / 2;
+
+        return array[index];
+    }
+
+    #endregion
+}
