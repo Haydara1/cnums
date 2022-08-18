95be19e2b7db67428d0398549587908c30119350
Haydara <haydaraal2005@gmail.com>
2022-08-18 13:48:54 -0700

    Created sqaure root class for handling symbolic coding.

---
+++ MathH/Symbolic coding classes/Symbolic functions/SSqrt.cs
@@ -0,0 +1,20 @@
+﻿namespace cnums;
+
+public class SSqrt
+{
+    private object? radicand = null;
+
+    public object? Radicand
+    {
+        get { return radicand; }
+        set { radicand = value; }   
+    }
+
+    public SSqrt(Symbol symbol) => radicand = symbol;
+
+    public SSqrt(double num) => radicand = num;
+
+
+
+    
+}
