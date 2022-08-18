43ed7f0cbbdd78b419a06bd911e2df287ec445dd
Haydara <haydaraal2005@gmail.com>
2022-08-18 13:45:52 -0700

    Added partial class for functions with symbols, implemeted power function.

---
+++ MathH/Maths functions/Symbolic coding external math functions/SymbolFunctions.cs
@@ -0,0 +1,8 @@
+﻿namespace cnums;
+
+public static partial class Maths
+{
+    public static Symbol Power(Symbol symbol, double power = 2)
+        => new(symbol.sym, symbol.coefficient, symbol.power * power);
+
+}
