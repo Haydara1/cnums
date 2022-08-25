a62063387f8322ce8615f60a386fce72d30753b2
Haydara <haydaraal2005@gmail.com>
2022-08-25 14:00:28 -0700

    Added summary, comments, and used equations.

--- MathH/SpecialFunctions.cs
+++ MathH/SpecialFunctions.cs
@@ -2,13 +2,19 @@
 
 namespace cnums
 {
+    /// <summary>
+    /// Includes a list of special functions used in mathematics.
+    /// </summary>
     public class SpecialFunctions
     {
+
         public static double Beta(double a, double b)
+           // Β(a, b) = (Γ(a) * Γ(b)) / Γ(a + b)
            => (Gamma(a) * Gamma(b)) / Gamma(a + b);
 
         public static double Gamma(double num)
         {
+            // Γ(z) = [z * exp(z * gamma) * Product(r = 0 -> inf){(1 + z / r) * exp(-z / r)}] ^ -1
 
             double product = Exp(Consts.Gamma * num);
             product *= num;
