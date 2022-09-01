e1216b3dd7383d9d579d7cf9e17731959711a573
Haydara <haydaraal2005@gmail.com>
2022-08-31 19:59:29 -0700

    Created class for divinding functions

---
+++ MathH/Symbolic coding classes/Fraction classes/ObjectFraction.cs
@@ -0,0 +1,26 @@
+﻿namespace cnums;
+
+internal class ObjectFraction : Fraction
+{
+    private object? numerator;
+
+    public new object? Numerator
+    {
+        get { return numerator; }
+        set { numerator = value; }
+    }
+    
+    private object? denominator;
+
+    public new object? Denominator
+    {
+        get { return denominator; }
+        set { denominator = value; }
+    }
+
+    public ObjectFraction(object obj1, object obj2) : base(0, 0)
+    {
+        Numerator = obj1;
+        Denominator = obj2;
+    }
+}
