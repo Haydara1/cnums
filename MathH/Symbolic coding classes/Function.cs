49d33f400f1bb951d8a0466ea4afbbfb55c48417
Haydara <haydaraal2005@gmail.com>
2022-08-31 19:58:49 -0700

    Added constructor taking a polynomial as a constructor parameter.

--- MathH/Symbolic coding classes/Function.cs
+++ MathH/Symbolic coding classes/Function.cs
@@ -1,10 +1,23 @@
-﻿namespace cnums
-{
-    public class Function
-    {
-        public Function(string equation, Symbol[] symbols)
-        {
+﻿namespace cnums;
 
-        }
+public class Function
+{
+
+    private object? function;
+
+    private Domain? domain;
+
+    public Domain? Domain
+    {
+        get { return domain; }
+        private set { domain = value; }
     }
+
+    public Function(Polynomial polynomial)
+    {
+        function = polynomial;
+        Domain = new();
+    }
+
 }
+
