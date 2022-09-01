f40a2f96aa78c2e60eb72b09203e629b0433417c
Haydara <haydaraal2005@gmail.com>
2022-08-31 20:02:59 -0700

    Removed power function for symbol class.

--- MathH/Maths functions/Symbolic coding external math functions/SymbolFunctions.cs
+++ MathH/Maths functions/Symbolic coding external math functions/SymbolFunctions.cs
@@ -2,7 +2,5 @@
 
 public static partial class Maths
 {
-    public static Symbol Power(Symbol symbol, double power = 2)
-        => new(symbol.sym, symbol.coefficient, symbol.power * power);
-
+    
 }
