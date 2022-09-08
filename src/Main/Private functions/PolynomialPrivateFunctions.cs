75cb1ae73ae9b42c12748f72946a9c1e51a9b14d
Haydara <haydaraal2005@gmail.com>
2022-09-08 16:24:53 -0700

    Created privare functions folder. Moved private functions of the polynomial class to the internal class PrivateFunctions.

---
+++ src/Main/Private functions/PolynomialPrivateFunctions.cs
@@ -0,0 +1,64 @@
+﻿namespace cnums;
+
+internal static partial class PrivateFunctions
+{
+    internal static List<Polynomial> DividePolynomial(Polynomial polynomial)
+    {
+        List<object> cn = polynomial.Container;
+        List<Polynomial> polynomials = new();
+        List<object> SinglePolynomial = new();
+
+        for (int i = 0; i < cn.Count; i++)
+        {
+            if (cn[i].ToString() == "+")
+            {
+                polynomials.Add(new(SinglePolynomial));
+                SinglePolynomial = new();
+                SinglePolynomial.Add('+');
+            }
+            else if (cn[i].ToString() == "-")
+            {
+                polynomials.Add(new(SinglePolynomial));
+                SinglePolynomial = new();
+                SinglePolynomial.Add('-');
+            }
+            else
+                SinglePolynomial.Add(cn[i]);
+        }
+
+        polynomials.Add(new(SinglePolynomial));
+
+        return polynomials;
+    }
+
+    internal static bool SingleTerm(Polynomial polynomial)
+    {
+        int index = 0;
+
+        foreach (object obj in polynomial.Container)
+            if (obj.ToString() == "+"
+                || obj.ToString() == "-")
+                index++;
+
+        return !(index > 1);
+    }
+
+    internal static bool ContainsSymbols(Polynomial polynomial)
+    {
+        for (int i = 0; i < polynomial.Container.Count; i++)
+            if (polynomial.Container[i].GetType() == typeof(cnums.Symbol))
+                return true;
+        return false;
+    }
+
+    internal static Symbol[] GetSymbols(Polynomial polynomial)
+    {
+        polynomial.SetDictionaries();
+        Dictionary<int, Symbol> symbols = polynomial.SymbolsDicitonary;
+
+        Symbol[] result = symbols.Values.ToArray();
+        result = result.Distinct().ToArray();
+
+        return result;
+    }
+}
---
+++ src/Main/Private functions/PrivateFunctions.cs
@@ -0,0 +1,139 @@
+﻿namespace cnums
+{
+    internal partial class PrivateFunctions
+    {
+        private static double Normalize(double num)
+        {
+            string number = num.ToString();
+
+            char[] numArray = number.ToCharArray();
+            List<char> numList = numArray.ToList(); //Convert number to char list.
+
+            if (numList.Contains('.')) numList.Remove('.'); //Remove '.' if found.
+            numList.Insert(1, '.'); //Add '.' at index one.
+
+            string str = String.Empty;
+
+            foreach (char c in numList) str += c; //Build list into one string.
+
+            return Convert.ToDouble(str);
+        }
+
+        private static int DigitCounter(double num)
+        {
+            string str = num.ToString();
+            int len = 0;
+
+            foreach (char c in str)
+            {
+                if (c == '.') break;
+                len++;
+            }
+
+            return len;
+        }
+
+        private static double[] GreaterThanOne(double num)
+        {
+            double[] arr = new double[2];
+
+            if (num < 1)
+            {
+                num = 1 / num;
+                arr[0] = num;
+                arr[1] = -1;
+
+                return arr;
+            }
+
+            arr[0] = num;
+            arr[1] = 1;
+
+            return arr;
+        }
+
+        private static double ListToDouble(List<int> list)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < list.Count; i++)
+                sum += list[i] * Maths.Power(10, -i);
+
+            return sum;
+        }
+
+        internal static double LogTen(double num, int accuracy = 100)
+        {
+            List<int> resultList = new();
+
+            double[] arr = GreaterThanOne(num);
+            num = arr[0];
+            double sign = arr[1];
+
+            for (int _ = 0; _ < accuracy; _++)
+            {
+                int DigitsNum = DigitCounter(num) - 1; //Depending on the eqaution: Number of digits = 1 + log(num)
+                resultList.Add(DigitsNum);
+
+                num = Normalize(num);
+                num = Maths.Power(num, 10);
+            }
+
+            double result = ListToDouble(resultList);
+            return sign * result; // log(a) = -log(1/a)
+        }
+
+        internal static double ModPi(double angle)
+            => angle % Consts.Tau;
+
+        internal static bool IsInteger(double number)
+            => number % 1 == 0;
+
+        internal static bool CrossedDomains(Domain domain1, Domain domain2)
+        {
+            if ((domain1.Begin >= domain2.Begin && domain1.Begin <= domain2.End) ||
+           (domain2.Begin >= domain1.Begin && domain2.Begin <= domain1.End)) 
+                return true;
+            return false;
+        }
+
+        internal static double AngleShift(double alpha, double beta)
+        {
+            double gamma;
+
+            if (alpha < beta)
+                gamma = beta - ((beta - alpha) % (2.0 * Consts.PI)) + 2.0 * Consts.PI;
+            else
+                gamma = beta + ((alpha - beta) % (2.0 * Consts.PI));
+
+            return gamma;
+        }
+
+        internal static double[,] MatricesMultiplication(double[,] arr1, double[,] arr2)
+        {
+            int rA = arr1.GetLength(0);
+            int cA = arr1.GetLength(1);
+            int rB = arr2.GetLength(0);
+            int cB = arr2.GetLength(1);
+
+            double temp;
+            double[,] result = new double[rA, cB];
+
+            if (cA != rB)
+                throw new Exception("Incompatible matrices.");
+            
+            for (int i = 0; i < rA; i++)
+            {
+                for (int j = 0; j < cB; j++)
+                {
+                    temp = 0;
+                    for (int k = 0; k < cA; k++)
+                        temp += arr1[i, k] * arr2[k, j];
+                        
+                    result[i, j] = temp;
+                }
+            }
+            return result;
+        }
+    }
+}
