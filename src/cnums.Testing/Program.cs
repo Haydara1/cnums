39681364ad4026055753463300f7bf76853bca69
Haydara <haydaraal2005@gmail.com>
2022-09-21 21:56:45 -0700

    Testing...

--- src/cnums.Testing/Program.cs
+++ src/cnums.Testing/Program.cs
@@ -1,15 +1,10 @@
 ﻿using cnums;
-using cnums.Symbolic;
 
 double[,] doubles = new double[,]
 {
-    {1, 3},
-    {2, 4 },
-    {3, 5},
+    {0 , 3, 4 },
+    {1 , 4, 5 },
 };
 
-Matrix matrix = new(doubles);
-matrix = Matrix.Transpose(matrix);
-for(int i = 0; i < matrix.Rows; i++)
-    for(int j = 0; j < matrix.Columns; j++)
-        Console.WriteLine(i + " " + matrix.matrix[i, j]);
+Matrix matrix = Matrix.Zeros(3, 5);
+Console.WriteLine(matrix.ToString());
