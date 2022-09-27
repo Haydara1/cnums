8f02a5ac9f1cafcb2eace811bb67420ef078fd70
Haydara <haydaraal2005@gmail.com>
2022-09-26 21:10:20 -0700

    Fixed indenting

--- src/cnums.Main/Matrix/MatrixFunctions.cs
+++ src/cnums.Main/Matrix/MatrixFunctions.cs
@@ -1,20 +1,18 @@
 ﻿// Some functions are inspired by the amazing python library, sympy,
 // the link for sympy's matrix: https://docs.sympy.org/latest/tutorials/intro-tutorial/matrices.html#.
 
-using System;
-
 namespace cnums;
 
 public static class MatrixFunctions
 {
     public static (int row, int col) Shape(this Matrix matrix)
         => (matrix.Rows, matrix.Columns);
-    
+
     public static double[] Row(this Matrix matrix, int index)
     {
         if (index == -1)
             index = matrix.Rows - 1;
-        else if(index >= matrix.Rows)
+        else if (index >= matrix.Rows)
             throw new IndexOutOfRangeException($"The matrix does not contain a row with index of {index}.");
 
         double[] result = new double[matrix.Columns];
@@ -42,17 +40,17 @@ public static class MatrixFunctions
 
     public static double Index(this Matrix matrix, int row, int col)
     {
-        if(row == -1) 
+        if (row == -1)
             row = matrix.Rows - 1;
 
-        if(col == -1)   
+        if (col == -1)
             col = matrix.Columns - 1;
 
         try
         {
             return matrix.matrix[row, col];
         }
-        catch(IndexOutOfRangeException)
+        catch (IndexOutOfRangeException)
         {
             throw new IndexOutOfRangeException($"The matrix does not contain an element with index of {row}, {col}.");
         }
