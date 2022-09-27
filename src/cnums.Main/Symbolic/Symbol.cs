4016a19908a5c68c877c8b75e8d4366490505aed
Haydara <haydaraal2005@gmail.com>
2022-09-26 21:13:43 -0700

    Fixed unary operator +, with a SymbolContainer return values now

--- src/cnums.Main/Symbolic/Symbol.cs
+++ src/cnums.Main/Symbolic/Symbol.cs
@@ -13,7 +13,7 @@
 
         public Symbol(char symbol)
         {
-            if (Char.IsDigit(symbol) 
+            if (Char.IsDigit(symbol)
                 || Char.IsPunctuation(symbol)
                 || Char.IsWhiteSpace(symbol))
                 throw new Exception("Symbol cannot be a digit or a punctuation mark or a white space.");
@@ -23,21 +23,21 @@
 
         public override bool Equals(object? obj)
         {
-            if (obj == null) 
+            if (obj == null)
                 return false;
 
-            if (obj.GetType() != this.GetType()) 
+            if (obj.GetType() != this.GetType())
                 return false;
 
             Symbol symbol = (Symbol)obj;
-            if (symbol.sym == this.sym) 
+            if (symbol.sym == this.sym)
                 return true;
 
             return false;
         }
 
         public override int GetHashCode()
-            => (int) sym;
+            => (int)sym;
 
         public override string ToString()
             => $"{this.Sym}";
@@ -45,20 +45,22 @@
         #region Addition.
 
         public static Polynomial operator +(Symbol symbol)
-            => new(new List<object> { symbol });
+            => new(new List<SymbolContainer>() { new SymbolContainer(symbol) });
 
         public static Polynomial operator +(Symbol symbol, double number)
-            => new(new List<object> { symbol, '+', number });
+            => new(new List<SymbolContainer>() { new SymbolContainer(symbol), new SymbolContainer(number) });
 
         public static Polynomial operator +(double number, Symbol symbol)
-            => new(new List<object> { number, '+', symbol });
+            => symbol + number;
 
         public static Polynomial operator +(Symbol symbol1, Symbol symbol2)
         {
-            if (symbol1 == symbol2)
-                return new(new List<object> { 2d, '*', symbol1 });
+            object addition = new SymbolContainer(symbol1) + new SymbolContainer(symbol2); 
 
-            return new(new List<object> { symbol1, '+', symbol2 });
+            if(addition.GetType() == typeof(cnums.SymbolContainer))
+                return new(new List<SymbolContainer>() { new SymbolContainer(symbol1), new SymbolContainer(symbol2) });
+
+            return new((Polynomial)addition, true);
         }
 
         #endregion
@@ -66,59 +68,61 @@
         #region Substraction
 
         public static Polynomial operator -(Symbol symbol)
-            => new(new List<object> { '-', symbol });
+            => new(new List<SymbolContainer>() { new SymbolContainer(symbol, coefficient: -1) });
 
         public static Polynomial operator -(Symbol symbol, double number)
-            => new(new List<object> { symbol, '-', number });
+            => new(new List<SymbolContainer>() { new SymbolContainer(symbol), new SymbolContainer(-number) });
 
         public static Polynomial operator -(double number, Symbol symbol)
-            => new(new List<object> { number, '-', symbol });
+            => new(new List<SymbolContainer>() { new SymbolContainer(symbol, coefficient: -1), new SymbolContainer(-number) });
 
         public static Polynomial operator -(Symbol symbol1, Symbol symbol2)
         {
-            if (symbol1 == symbol2)
-                return new(new List<object> { 0d });
+            object substraction = new SymbolContainer(symbol1) - new SymbolContainer(symbol2);
 
-            return new(new List<object> { symbol1, '-', symbol2 });
+            if (substraction.GetType() == typeof(cnums.SymbolContainer))
+                return new(new List<SymbolContainer>() { (SymbolContainer)substraction });
+
+            return new((Polynomial)substraction, true);
         }
 
         #endregion
 
-        #region Multiplication
+        //#region Multiplication
 
-        public static Polynomial operator *(Symbol symbol, double number)
-            => new(new List<object> { number, '*', symbol });
+        //public static Polynomial operator *(Symbol symbol, double number)
+        //    => new(new List<object> { number, '*', symbol });
 
-        public static Polynomial operator *(double number, Symbol symbol)
-            => new(new List<object> { number, '*', symbol });
+        //public static Polynomial operator *(double number, Symbol symbol)
+        //    => new(new List<object> { number, '*', symbol });
 
-        public static Polynomial operator *(Symbol symbol1, Symbol symbol2)
-        {
-            if (symbol1 == symbol2)
-                return new(new List<object> { symbol1, '^', 2 });
+        //public static Polynomial operator *(Symbol symbol1, Symbol symbol2)
+        //{
+        //    if (symbol1 == symbol2)
+        //        return new(new List<object> { symbol1, '^', 2 });
 
-            return new(new List<object> { symbol1, '*', symbol2 });
-        }
+        //    return new(new List<object> { symbol1, '*', symbol2 });
+        //}
 
-        #endregion
+        //#endregion
 
-        #region Division
+        //#region Division
 
-        public static Polynomial operator /(Symbol symbol, double number)
-            => new(new List<object> { symbol, '/', number });
+        //public static Polynomial operator /(Symbol symbol, double number)
+        //    => new(new List<object> { symbol, '/', number });
 
-        public static Polynomial operator /(double number, Symbol symbol)
-            => new(new List<object> { number, '/', symbol });
+        //public static Polynomial operator /(double number, Symbol symbol)
+        //    => new(new List<object> { number, '/', symbol });
 
-        public static Polynomial operator /(Symbol symbol1, Symbol symbol2)
-        {
-            if(symbol1 == symbol2)
-                return new Polynomial(new List<object> { 1d });
+        //public static Polynomial operator /(Symbol symbol1, Symbol symbol2)
+        //{
+        //    if (symbol1 == symbol2)
+        //        return new Polynomial(new List<object> { 1d });
 
-            return new(new List<object> { symbol1, '/', symbol2 });
-        }
+        //    return new(new List<object> { symbol1, '/', symbol2 });
+        //}
 
-        #endregion
+        //#endregion
 
         #region Comparision
 
