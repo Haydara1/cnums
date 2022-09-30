1c96fde283afb803106b40016d3262ed9dd58cec
Haydara <haydaraal2005@gmail.com>
2022-09-30 16:35:23 -0700

    Added unicode printing

--- src/cnums.Main/Symbolic/Base functions/SymbolContainer.cs
+++ src/cnums.Main/Symbolic/Base functions/SymbolContainer.cs
@@ -10,9 +10,7 @@ internal class SymbolContainer
 
     public List<double> Exponent = new() { };
 
-    public bool Negative = false;
-
-    public Dictionary<Symbol, double> SymExp = new();
+    #region Constructors
 
     public SymbolContainer(Symbol symbol, double coefficient, double exponent)
     {
@@ -29,11 +27,7 @@ internal class SymbolContainer
     }
 
     public SymbolContainer(double number)
-    {
-        Coefficient = number;
-        Exponent.Add(0);
-        symbol.Add(new('x'));
-    }
+        => Coefficient = number;
 
     public SymbolContainer(Symbol symbol)
     {
@@ -49,24 +43,118 @@ internal class SymbolContainer
         this.symbol.Add(symbol);
     }
 
+    #endregion
+
     public override string ToString()
     {
-        string result = $"{Coefficient}";
+        Clean();
+
+        if (isNull())
+            return "";
+        else if (isConstant())
+            return Coefficient > 0 ? "+" + Maths.Abs(Coefficient).ToString() :
+                                     "-" + Maths.Abs(Coefficient).ToString();
+
+        if (Utils.Unicode)
+            return ToUnicodeString();
+
+        string result;
+
+        if (Coefficient is 1)
+            result = "+";
+        else if (Coefficient is -1)
+            result = "-";
+        else 
+            result = Coefficient < 0 ? Coefficient.ToString() : "+" + Coefficient.ToString();
 
         for (int i = 0; i < symbol.Count; i++)
-            result += $"*{symbol[i]}^{Exponent[i]}";
+        {
+            if(result is "+" or "-")
+                result += Exponent[i] == 1 ? symbol[i] : $"{symbol[i]}^{Exponent[i]}";
+            else
+                result += Exponent[i] == 1 ? "*" + symbol[i] : $"*{symbol[i]}^{Exponent[i]}";
+        }   
 
         return result;
     }
+
+    public string ToUnicodeString()
+    {
+        string result;
+
+        if (Coefficient is 1)
+            result = "+";
+        else if (Coefficient is -1)
+            result = "-";
+        else
+            result = Coefficient < 0 ? Coefficient.ToString() : "+" + Coefficient.ToString();
+
+        for (int i = 0; i < symbol.Count; i++)
+        {
+            if (result is "+" or "-")
+                result += Exponent[i] == 1 ? symbol[i] : $"{symbol[i]}{Exponent[i].UnicodeExponent()}";
+            else
+                result += Exponent[i] == 1 ? symbol[i] : $"{symbol[i]}{Exponent[i].UnicodeExponent()}";
+        }
+
+        return result;
+    }
+
+    private static bool equalExpSym(List<Symbol> sym1, 
+                                    List<Symbol> sym2, 
+                                    List<double> exp1, 
+                                    List<double> exp2)
+    {
+        if (sym1.Count != sym2.Count)
+            return false;
+
+        for(int i = 0; i < sym1.Count; i++)
+        {
+            if (!sym2.Contains(sym1[i]))
+                return false;
+
+            if (exp1[i] != exp2[sym2.IndexOf(sym1[i])])
+                return false;
+        }
+
+        return true;
+    }
+
+    private void Clean()
+    {
+        for(int i = 0; i < this.Exponent.Count; i++)
+            if (this.Exponent[i] == 0)
+            {
+                this.Exponent.RemoveAt(i);
+                this.symbol.RemoveAt(i);
+                i--;
+            }
+    }
+
+    internal bool isConstant()
+    {
+        foreach(double exponent in Exponent)
+            if(exponent != 0)
+                return false;
+
+        return true;
+    }
+
+    private bool isNull()
+        => Coefficient == 0;
+
+    private bool isNegative()
+        => Coefficient < 0;
+
+    #region Addition
 
     public static SymbolContainer operator +(SymbolContainer symbolContainer)
         => symbolContainer;
 
     public static object operator +(SymbolContainer symbolContainer, double num)
     {
-        for(int i = 0; i < symbolContainer.Exponent.Count; i++)
-            if (symbolContainer.Exponent[i] != 0)
-                return new Polynomial(new List<SymbolContainer>() { symbolContainer, new SymbolContainer(num) });
+        if (!symbolContainer.isConstant())
+            return new Polynomial(new List<SymbolContainer>() { symbolContainer, new SymbolContainer(num) });
 
         return new SymbolContainer(symbolContainer.symbol,
             symbolContainer.Coefficient + num,
@@ -87,14 +175,18 @@ internal class SymbolContainer
 
     public static object operator +(SymbolContainer symbolContainer1, SymbolContainer symbolContainer2)
     {
-        if (symbolContainer1.symbol == symbolContainer2.symbol
-            && symbolContainer1.Exponent == symbolContainer2.Exponent)
+        if (equalExpSym(symbolContainer1.symbol, symbolContainer2.symbol,
+                        symbolContainer1.Exponent, symbolContainer2.Exponent))
             return new SymbolContainer(symbolContainer1.symbol,
                 symbolContainer1.Coefficient + symbolContainer2.Coefficient,
                 symbolContainer1.Exponent);
 
         return new Polynomial(new List<SymbolContainer>() { symbolContainer1, symbolContainer2 });
     }
+
+    #endregion
+
+    #region Substraction
 
     public static SymbolContainer operator -(SymbolContainer symbolContainer)
         => new(symbolContainer.symbol, -symbolContainer.Coefficient, symbolContainer.Exponent);
@@ -124,8 +216,8 @@ internal class SymbolContainer
 
     public static object operator -(SymbolContainer symbolContainer1, SymbolContainer symbolContainer2)
     {
-        if (symbolContainer1.symbol == symbolContainer2.symbol
-            && symbolContainer1.Exponent == symbolContainer2.Exponent)
+        if (equalExpSym(symbolContainer1.symbol, symbolContainer2.symbol,
+                        symbolContainer1.Exponent, symbolContainer2.Exponent))
             return new SymbolContainer(symbolContainer1.symbol,
                 symbolContainer1.Coefficient - symbolContainer2.Coefficient,
                 symbolContainer1.Exponent);
@@ -133,11 +225,12 @@ internal class SymbolContainer
         return new Polynomial(new List<SymbolContainer>() { symbolContainer1, -symbolContainer2 });
     }
 
+    #endregion
+
+    #region Multiplication
+
     public static SymbolContainer operator *(SymbolContainer symbolContainer, double num)
         => new(symbolContainer.symbol, symbolContainer.Coefficient * num, symbolContainer.Exponent);
-
-    public static SymbolContainer operator /(SymbolContainer symbolContainer, double num)
-        => new(symbolContainer.symbol, symbolContainer.Coefficient / num, symbolContainer.Exponent);
 
     public static SymbolContainer operator *(SymbolContainer symbolContainer, Symbol symbol)
     {
@@ -151,4 +244,36 @@ internal class SymbolContainer
 
         return new(symbolContainer.symbol, symbolContainer.Coefficient, symbolContainer.Exponent);
     }
+
+    public static SymbolContainer operator *(SymbolContainer symbolContainer1, SymbolContainer symbolContainer2)
+    {
+        List<Symbol> symList = symbolContainer1.symbol;
+        List<double> expList = symbolContainer1.Exponent;
+
+        for(int i = 0; i < symbolContainer2.symbol.Count; i++)
+        {
+            if (symList.Contains(symbolContainer2.symbol[i]))
+                expList[symList.IndexOf(
+                    symbolContainer2.symbol[i])] += symbolContainer2.Exponent[i];
+            else
+            {
+                symList.Add(symbolContainer2.symbol[i]);
+                expList.Add(symbolContainer2.Exponent[i]);
+            }
+        }
+
+        return new(symList,
+            symbolContainer1.Coefficient * symbolContainer2.Coefficient,
+            expList);
+    }
+
+    #endregion
+
+    #region Division
+
+    public static SymbolContainer operator /(SymbolContainer symbolContainer, double num)
+        => new(symbolContainer.symbol, symbolContainer.Coefficient / num, symbolContainer.Exponent);
+    
+
+    #endregion
 }
