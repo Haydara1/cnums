2653e71d1157ce7af8b5eae2fd4bd9f7e9a36aab
Haydara <haydaraal2005@gmail.com>
2022-09-30 17:21:24 -0700

    Reimplemented operators with better algorithms

--- src/cnums.Main/Symbolic/Base functions/Polynomial.cs
+++ src/cnums.Main/Symbolic/Base functions/Polynomial.cs
@@ -30,17 +30,143 @@ public class Polynomial
         return poly;
     }
 
+    private bool ContainsConstant()
+    {
+        foreach (SymbolContainer symbolContainer in this.Container)
+            if (symbolContainer.isConstant())
+                return true;
+
+        return false;
+    }
+
+    private int getConstantIndex()
+    {
+        foreach (SymbolContainer symbolContainer in this.Container)
+            if (symbolContainer.isConstant())
+                return Container.IndexOf(symbolContainer);
+
+        return -1;
+    }
+
+    private bool ContainsSymbol(Symbol symbol)
+    {
+        foreach (SymbolContainer symbolContainer in this.Container)
+            if (symbolContainer.symbol.Contains(symbol))
+                return true;
+
+        return false;
+    }
+
+    private int FindTerm(Symbol symbol, double exponent)
+    {
+        foreach (SymbolContainer symbolContainer in this.Container)
+            if (symbolContainer.symbol.Count is 1
+                && symbolContainer.symbol[0] == symbol
+                && symbolContainer.Exponent[0] == exponent)
+                return this.Container.IndexOf(symbolContainer);
+
+        return -1;
+    }
+
+    #region Addition
+
     public static Polynomial operator +(Polynomial polynomial)
         => polynomial;
 
-    public static Polynomial operator -(Polynomial polynomial)
+    public static Polynomial operator +(Polynomial polynomial, double number)
     {
-        for (int i = 0; i < polynomial.Container.Count; i++)
-            polynomial.Container[i] = -polynomial.Container[i];
-
-        return polynomial;
+        var container = polynomial.Container;
+        if (polynomial.ContainsConstant())
+            container[polynomial.getConstantIndex()] = (SymbolContainer)
+                (polynomial.Container[polynomial.getConstantIndex()] + number);
+        else
+            container.Add(new SymbolContainer(number));
+        
+        return new(container);
     }
 
-}
+    public static Polynomial operator +(double number, Polynomial polynomial)
+        => polynomial + number;
 
-    
+    public static Polynomial operator +(Polynomial polynomial, Symbol symbol)
+    {
+        var container = polynomial.Container;
+        if (polynomial.ContainsSymbol(symbol))
+            container[polynomial.FindTerm(symbol, 1)].Coefficient++;
+        else
+            container.Add(new SymbolContainer(symbol));
+
+        return new(container);
+    }
+
+    public static Polynomial operator +(Symbol symbol, Polynomial polynomial)
+        => polynomial + symbol;
+
+    #endregion
+
+
+    #region Substraction
+
+    public static Polynomial operator -(Polynomial polynomial)
+    {
+        var container = polynomial.Container;
+        for (int i = 0; i < container.Count; i++)
+            container[i] = -container[i];
+
+        return new(container);
+    }
+
+    public static Polynomial operator -(Polynomial polynomial, double number)
+    {
+        var container = polynomial.Container;
+        if (polynomial.ContainsConstant())
+            container[polynomial.getConstantIndex()] = (SymbolContainer)
+                (polynomial.Container[polynomial.getConstantIndex()] - number);
+        else
+            container.Add(new SymbolContainer(-number));
+
+        return new(container);
+    }
+
+    public static Polynomial operator -(double number, Polynomial polynomial)
+        => -(-number + polynomial);
+
+    public static Polynomial operator -(Polynomial polynomial, Symbol symbol)
+    {
+        var container = polynomial.Container;
+        if (polynomial.ContainsSymbol(symbol))
+            container[polynomial.FindTerm(symbol, 1)].Coefficient--;
+        else
+            container.Add(-new SymbolContainer(symbol));
+
+        return new(container);
+    }
+
+    //public static Polynomial operator -(Symbol symbol, Polynomial polynomial)
+    //    => -(-symbol + polynomial);
+
+    #endregion
+
+    #region Multiplication
+
+    public static Polynomial operator *(Polynomial polynomial, double number)
+    {
+        List<SymbolContainer> container = polynomial.Container;
+        for (int i = 0; i < container.Count; i++)
+            container[i] *= number;
+
+        return new(container);
+    }
+
+    public static Polynomial operator *(Polynomial polynomial, Symbol symbol)
+    {
+        List<SymbolContainer> container = polynomial.Container;
+        for (int i = 0; i < container.Count; i++)
+            container[i] *= symbol;
+
+        return new(container);
+    }
+
+
+    #endregion
+}
