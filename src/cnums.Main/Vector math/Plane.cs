4aa55f1cf53817b285b8cb231284fc9c2e2bac39
Haydara <haydaraal2005@gmail.com>
2022-10-02 20:09:23 -0700

    Added plane in a 3D space object

---
+++ src/cnums.Main/Vector math/Plane.cs
@@ -0,0 +1,22 @@
+﻿namespace cnums.Vector_math;
+
+public struct Plane
+{
+    private readonly double a;
+    private readonly double b;
+    private readonly double c;
+    private readonly double d;
+
+    public double A => a;
+    public double B => b;
+    public double C => c;
+    public double D => d;
+
+    public Plane(double a, double b, double c, double d)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+}
