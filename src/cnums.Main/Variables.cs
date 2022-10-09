cc04a101fd0b2d342fb316b13a7d124d44745050
Haydara <haydaraal2005@gmail.com>
2022-10-08 21:11:00 -0700

    Added summaries

--- src/cnums.Main/Variables.cs
+++ src/cnums.Main/Variables.cs
@@ -4,95 +4,401 @@ using cnums.Symbolic;
 
 namespace cnums;
 
+/// <summary>
+/// Provides most of variable names that can be declared for symbols, in both Latin and Greek aphabets.
+/// </summary>
 public static class Variables
 {
     #region Lower case Latin letters
 
+    /// <summary>
+    /// Lower case letter a
+    /// </summary>
     public static readonly Symbol a = new('a');
+
+    /// <summary>
+    /// Lower case letter b
+    /// </summary>
     public static readonly Symbol b = new('b');
+
+    /// <summary>
+    /// Lower case letter c
+    /// </summary>
     public static readonly Symbol c = new('c');
+
+    /// <summary>
+    /// Lower case letter d
+    /// </summary>
     public static readonly Symbol d = new('d');
+
+    /// <summary>
+    /// Lower case letter e
+    /// </summary>
     public static readonly Symbol e = new('e');
+
+    /// <summary>
+    /// Lower case letter f
+    /// </summary>
     public static readonly Symbol f = new('f');
+
+    /// <summary>
+    /// Lower case letter g
+    /// </summary>
     public static readonly Symbol g = new('g');
+
+    /// <summary>
+    /// Lower case letter h
+    /// </summary>
     public static readonly Symbol h = new('h');
+
+    /// <summary>
+    /// Lower case letter i
+    /// </summary>
     public static readonly Symbol i = new('i');
+
+    /// <summary>
+    /// Lower case letter j
+    /// </summary>
     public static readonly Symbol j = new('j');
+
+    /// <summary>
+    /// Lower case letter k
+    /// </summary>
     public static readonly Symbol k = new('k');
+
+    /// <summary>
+    /// Lower case letter l
+    /// </summary>
     public static readonly Symbol l = new('l');
+
+    /// <summary>
+    /// Lower case letter m
+    /// </summary>
     public static readonly Symbol m = new('m');
+
+    /// <summary>
+    /// Lower case letter n
+    /// </summary>
     public static readonly Symbol n = new('n');
+
+    /// <summary>
+    /// Lower case letter o
+    /// </summary>
     public static readonly Symbol o = new('o');
+
+    /// <summary>
+    /// Lower case letter p
+    /// </summary>
     public static readonly Symbol p = new('p');
+
+    /// <summary>
+    /// Lower case letter q
+    /// </summary>
     public static readonly Symbol q = new('q');
+
+    /// <summary>
+    /// Lower case letter r
+    /// </summary>
     public static readonly Symbol r = new('r');
+
+    /// <summary>
+    /// Lower case letter s
+    /// </summary>
     public static readonly Symbol s = new('s');
+
+    /// <summary>
+    /// Lower case letter v
+    /// </summary>
     public static readonly Symbol v = new('v');
+
+    /// <summary>
+    /// Lower case letter u
+    /// </summary>
     public static readonly Symbol u = new('u');
+
+    /// <summary>
+    /// Lower case letter t
+    /// </summary>
     public static readonly Symbol t = new('t');
+
+    /// <summary>
+    /// Lower case letter w
+    /// </summary>
     public static readonly Symbol w = new('w');
+
+    /// <summary>
+    /// Lower case letter x
+    /// </summary>
     public static readonly Symbol x = new('x');
+
+    /// <summary>
+    /// Lower case letter y
+    /// </summary>
     public static readonly Symbol y = new('y');
+
+    /// <summary>
+    /// Lower case letter z
+    /// </summary>
     public static readonly Symbol z = new('z');
+
 
     #endregion
 
     #region Upper case Latin letters
 
+    /// <summary>
+    /// Upper case letter A
+    /// </summary>
     public static readonly Symbol A = new('A');
+
+    /// <summary>
+    /// Upper case letter B
+    /// </summary>
     public static readonly Symbol B = new('B');
+
+    /// <summary>
+    /// Upper case letter C
+    /// </summary>
     public static readonly Symbol C = new('C');
+
+    /// <summary>
+    /// Upper case letter D
+    /// </summary>
     public static readonly Symbol D = new('D');
+
+    /// <summary>
+    /// Upper case letter E
+    /// </summary>
     public static readonly Symbol E = new('E');
+
+    /// <summary>
+    /// Upper case letter F
+    /// </summary>
     public static readonly Symbol F = new('F');
+
+    /// <summary>
+    /// Upper case letter G
+    /// </summary>
     public static readonly Symbol G = new('G');
+
+    /// <summary>
+    /// Upper case letter H
+    /// </summary>
     public static readonly Symbol H = new('H');
+
+    /// <summary>
+    /// Upper case letter I
+    /// </summary>
     public static readonly Symbol I = new('I');
+
+    /// <summary>
+    /// Upper case letter J
+    /// </summary>
     public static readonly Symbol J = new('J');
+
+    /// <summary>
+    /// Upper case letter K
+    /// </summary>
     public static readonly Symbol K = new('K');
+
+    /// <summary>
+    /// Upper case letter L
+    /// </summary>
     public static readonly Symbol L = new('L');
+
+    /// <summary>
+    /// Upper case letter M
+    /// </summary>
     public static readonly Symbol M = new('M');
+
+    /// <summary>
+    /// Upper case letter N
+    /// </summary>
     public static readonly Symbol N = new('N');
+
+    /// <summary>
+    /// Upper case letter O
+    /// </summary>
     public static readonly Symbol O = new('O');
+
+    /// <summary>
+    /// Upper case letter P
+    /// </summary>
     public static readonly Symbol P = new('P');
+
+    /// <summary>
+    /// Upper case letter Q
+    /// </summary>
     public static readonly Symbol Q = new('Q');
+
+    /// <summary>
+    /// Upper case letter R
+    /// </summary>
     public static readonly Symbol R = new('R');
+
+    /// <summary>
+    /// Upper case letter S
+    /// </summary>
     public static readonly Symbol S = new('S');
+
+    /// <summary>
+    /// Upper case letter V
+    /// </summary>
     public static readonly Symbol V = new('V');
+
+    /// <summary>
+    /// Upper case letter U
+    /// </summary>
     public static readonly Symbol U = new('U');
+
+    /// <summary>
+    /// Upper case letter T
+    /// </summary>
     public static readonly Symbol T = new('T');
+
+    /// <summary>
+    /// Upper case letter W
+    /// </summary>
     public static readonly Symbol W = new('W');
+
+    /// <summary>
+    /// Upper case letter X
+    /// </summary>
     public static readonly Symbol X = new('X');
+
+    /// <summary>
+    /// Upper case letter Y
+    /// </summary>
     public static readonly Symbol Y = new('Y');
+
+    /// <summary>
+    /// Upper case letter Z
+    /// </summary>
     public static readonly Symbol Z = new('Z');
+
 
     #endregion
 
     #region Greek letters
 
+    /// <summary>
+    /// Lower case greek letter alpha (α)
+    /// </summary>
     public static readonly Symbol alpha = new('α');
+
+    /// <summary>
+    /// Lower case greek letter beta (β)
+    /// </summary>
     public static readonly Symbol beta = new('β');
+
+    /// <summary>
+    /// Lower case greek letter gamma (γ)
+    /// </summary>
     public static readonly Symbol gamma = new('γ');
+
+    /// <summary>
+    /// Lower case greek letter delta (δ)
+    /// </summary>
     public static readonly Symbol delta = new('δ');
+
+    /// <summary>
+    /// Lower case greek letter epsilon (ε)
+    /// </summary>
     public static readonly Symbol epsilon = new('ε');
+
+    /// <summary>
+    /// Lower case greek letter zeta (ζ)
+    /// </summary>
     public static readonly Symbol zeta = new('ζ');
+
+    /// <summary>
+    /// Lower case greek letter eta (η)
+    /// </summary>
     public static readonly Symbol eta = new('η');
+
+    /// <summary>
+    /// Lower case greek letter theta (θ)
+    /// </summary>
     public static readonly Symbol theta = new('θ');
+
+    /// <summary>
+    /// Lower case greek letter iota (ι)
+    /// </summary>
     public static readonly Symbol iota = new('ι');
+
+    /// <summary>
+    /// Lower case greek letter kappa (κ)
+    /// </summary>
     public static readonly Symbol kappa = new('κ');
+
+    /// <summary>
+    /// Lower case greek letter lambda (λ)
+    /// </summary>
     public static readonly Symbol lambda = new('λ');
+
+    /// <summary>
+    /// Lower case greek letter mu (μ)
+    /// </summary>
     public static readonly Symbol mu = new('μ');
+
+    /// <summary>
+    /// Lower case greek letter nu (ν)
+    /// </summary>
     public static readonly Symbol nu = new('ν');
+
+    /// <summary>
+    /// Lower case greek letter xi (ξ)
+    /// </summary>
     public static readonly Symbol xi = new('ξ');
+
+    /// <summary>
+    /// Lower case greek letter omicron (ο)
+    /// </summary>
     public static readonly Symbol omicron = new('ο');
+
+    /// <summary>
+    /// Lower case greek letter pi (π)
+    /// </summary>
     public static readonly Symbol pi = new('π');
+
+    /// <summary>
+    /// Lower case greek letter rho (ρ)
+    /// </summary>
     public static readonly Symbol rho = new('ρ');
+
+    /// <summary>
+    /// Lower case greek letter sigma (σ)
+    /// </summary>
     public static readonly Symbol sigma = new('σ');
+
+    /// <summary>
+    /// Lower case greek letter tau (τ)
+    /// </summary>
     public static readonly Symbol tau = new('τ');
+
+    /// <summary>
+    /// Lower case greek letter upsilon (υ)
+    /// </summary>
     public static readonly Symbol upsilon = new('υ');
+
+    /// <summary>
+    /// Lower case greek letter chi (ψ)
+    /// </summary>
     public static readonly Symbol chi = new('ψ');
+
+    /// <summary>
+    /// Lower case greek letter psi (χ)
+    /// </summary>
     public static readonly Symbol psi = new('χ');
+
+    /// <summary>
+    /// Lower case greek letter phi (φ)
+    /// </summary>
     public static readonly Symbol phi = new('φ');
+
+    /// <summary>
+    /// Lower case greek letter omega (ω)
+    /// </summary>
     public static readonly Symbol omega = new('ω');
 
     #endregion
