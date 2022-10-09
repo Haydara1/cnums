a8bbdd05d4381c7e35eaacf42d2b1a62de6053d4
Haydara <haydaraal2005@gmail.com>
2022-10-08 20:45:24 -0700

    Added class containing most of the symbols possible names instead of declaring new ones

---
+++ src/cnums.Main/Variables.cs
@@ -0,0 +1,99 @@
+﻿// Inspiration: sympy.abc
+
+using cnums.Symbolic;
+
+namespace cnums;
+
+public static class Variables
+{
+    #region Lower case Latin letters
+
+    public static readonly Symbol a = new('a');
+    public static readonly Symbol b = new('b');
+    public static readonly Symbol c = new('c');
+    public static readonly Symbol d = new('d');
+    public static readonly Symbol e = new('e');
+    public static readonly Symbol f = new('f');
+    public static readonly Symbol g = new('g');
+    public static readonly Symbol h = new('h');
+    public static readonly Symbol i = new('i');
+    public static readonly Symbol j = new('j');
+    public static readonly Symbol k = new('k');
+    public static readonly Symbol l = new('l');
+    public static readonly Symbol m = new('m');
+    public static readonly Symbol n = new('n');
+    public static readonly Symbol o = new('o');
+    public static readonly Symbol p = new('p');
+    public static readonly Symbol q = new('q');
+    public static readonly Symbol r = new('r');
+    public static readonly Symbol s = new('s');
+    public static readonly Symbol v = new('v');
+    public static readonly Symbol u = new('u');
+    public static readonly Symbol t = new('t');
+    public static readonly Symbol w = new('w');
+    public static readonly Symbol x = new('x');
+    public static readonly Symbol y = new('y');
+    public static readonly Symbol z = new('z');
+
+    #endregion
+
+    #region Upper case Latin letters
+
+    public static readonly Symbol A = new('A');
+    public static readonly Symbol B = new('B');
+    public static readonly Symbol C = new('C');
+    public static readonly Symbol D = new('D');
+    public static readonly Symbol E = new('E');
+    public static readonly Symbol F = new('F');
+    public static readonly Symbol G = new('G');
+    public static readonly Symbol H = new('H');
+    public static readonly Symbol I = new('I');
+    public static readonly Symbol J = new('J');
+    public static readonly Symbol K = new('K');
+    public static readonly Symbol L = new('L');
+    public static readonly Symbol M = new('M');
+    public static readonly Symbol N = new('N');
+    public static readonly Symbol O = new('O');
+    public static readonly Symbol P = new('P');
+    public static readonly Symbol Q = new('Q');
+    public static readonly Symbol R = new('R');
+    public static readonly Symbol S = new('S');
+    public static readonly Symbol V = new('V');
+    public static readonly Symbol U = new('U');
+    public static readonly Symbol T = new('T');
+    public static readonly Symbol W = new('W');
+    public static readonly Symbol X = new('X');
+    public static readonly Symbol Y = new('Y');
+    public static readonly Symbol Z = new('Z');
+
+    #endregion
+
+    #region Greek letters
+
+    public static readonly Symbol alpha = new('α');
+    public static readonly Symbol beta = new('β');
+    public static readonly Symbol gamma = new('γ');
+    public static readonly Symbol delta = new('δ');
+    public static readonly Symbol epsilon = new('ε');
+    public static readonly Symbol zeta = new('ζ');
+    public static readonly Symbol eta = new('η');
+    public static readonly Symbol theta = new('θ');
+    public static readonly Symbol iota = new('ι');
+    public static readonly Symbol kappa = new('κ');
+    public static readonly Symbol lambda = new('λ');
+    public static readonly Symbol mu = new('μ');
+    public static readonly Symbol nu = new('ν');
+    public static readonly Symbol xi = new('ξ');
+    public static readonly Symbol omicron = new('ο');
+    public static readonly Symbol pi = new('π');
+    public static readonly Symbol rho = new('ρ');
+    public static readonly Symbol sigma = new('σ');
+    public static readonly Symbol tau = new('τ');
+    public static readonly Symbol upsilon = new('υ');
+    public static readonly Symbol chi = new('ψ');
+    public static readonly Symbol psi = new('χ');
+    public static readonly Symbol phi = new('φ');
+    public static readonly Symbol omega = new('ω');
+
+    #endregion
+}
