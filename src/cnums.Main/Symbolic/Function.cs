3506b81f9b308e20c38a7e4c3f271d8e84cd319d
Haydara <haydaraal2005@gmail.com>
2022-10-10 21:24:58 -0700

    Added constructors and operators

--- src/cnums.Main/Symbolic/Function.cs
+++ src/cnums.Main/Symbolic/Function.cs
@@ -3,7 +3,7 @@
 public class Function
 {
 
-    private object? function;
+    private object function;
 
     private Domain? domain;
 
@@ -13,11 +13,83 @@ public class Function
         private set { domain = value; }
     }
 
-    public Function(Polynomial polynomial)
+    #region  Constructors
+
+    internal Function(Polynomial polynomial)
     {
         function = polynomial;
         Domain = new();
     }
+
+    internal Function(Symbol symbol)
+    {
+        function = symbol;
+        Domain = new();
+    }
+
+    internal Function(double value)
+    {
+        function = value;
+        Domain = new();
+    }
+
+    internal Function(int value)
+    {
+        function = value;
+        Domain = new();
+    }
+
+    #endregion 
+
+    public override string? ToString()
+        => function.ToString();
+
+    #region Addition
+
+    public static Function operator +(Function function)
+        => function;
+
+    public static Function operator +(Function function, double number)
+    {
+        if (function.function.GetType() == typeof(double))
+            return new((double)function.function + number);
+
+        else if (function.function.GetType() == typeof(int))
+            return new((int)function.function + number);
+
+        else if (function.function.GetType() == typeof(Polynomial))
+            return new((Polynomial)function.function + number);
+
+        else if (function.function.GetType() == typeof(Symbol))
+            return new((Symbol)function.function + number);
+
+        throw new CnumsException();
+    }
+
+    public static Function operator +(double number, Function function)
+        => function + number;
+
+    public static Function operator +(Function function, Symbol symbol)
+    {
+        if (function.function.GetType() == typeof(double))
+            return new((double)function.function + symbol);
+
+        else if (function.function.GetType() == typeof(int))
+            return new((int)function.function + symbol);
+
+        else if (function.function.GetType() == typeof(Polynomial))
+            return new((Polynomial)function.function + symbol);
+
+        else if (function.function.GetType() == typeof(Symbol))
+            return new((Symbol)function.function + symbol);
+
+        throw new CnumsException();
+    }
+
+    public static Function operator +(Symbol symbol, Function function)
+        => function + symbol;
+
+    #endregion
 
 }
 
