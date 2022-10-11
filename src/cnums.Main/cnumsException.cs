90d27ec7952ce0146fc2dfa1ac8862175b4bbc12
Haydara <haydaraal2005@gmail.com>
2022-10-10 21:23:40 -0700

    Added a unique exception for the library

---
+++ src/cnums.Main/cnumsException.cs
@@ -0,0 +1,25 @@
+﻿namespace cnums;
+
+public class CnumsException : Exception 
+{
+    //Overriding the Message property
+    public override string Message
+    {
+        get
+        {
+            return "This exception was caused by a bug in the namespace, it is us, not you!";
+        }
+    }
+
+    //Overriding the HelpLink Property
+    public override string HelpLink
+    {
+        get
+        {
+            return "It is probably marked as an issue: https://github.com/Haydara1/cnums/issues, otherwise, please let us know about it!";
+        }
+    }
+
+    public CnumsException() : base()
+    { }
+}
