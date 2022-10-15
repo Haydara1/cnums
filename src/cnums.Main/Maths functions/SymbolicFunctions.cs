9742815d0ef64ad91ba78b50e5f442c96dcd9fe3
Haydara <haydaraal2005@gmail.com>
2022-10-14 21:00:34 -0700

    Removed from folder

---
+++ src/cnums.Main/Maths functions/SymbolicFunctions.cs
@@ -0,0 +1,14 @@
+﻿using cnums.Symbolic;
+
+namespace cnums;
+
+public static partial class Maths
+{   
+
+    public static Function Power(Symbol symbol, int power)
+        // Transforms the symbol to a new symbol container whose coefficient is one, and its exponent is the power.
+        // Then transfor the symbol container to Polynomial then to Function.
+        => new Polynomial(new() { new(symbol, coefficient: 1, exponent: power) }).ToFunction();
+    
+
+}
