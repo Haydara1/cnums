3e515f44219e2850f2a7e57434c7fa3ed1bd9d09
Haydara <haydaraal2005@gmail.com>
2022-10-16 21:18:34 -0700

    Changed folder

---
+++ src/cnums.Main/Symbolic/Symbolic functions/Sqrt.cs
@@ -0,0 +1,30 @@
+﻿namespace cnums.Symbolic;
+
+public class Sqrt
+{
+    #region Properties
+
+    private object? radicand = null;
+
+    public object? Radicand
+    {
+        get { return radicand; }
+        set { radicand = value; }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public Sqrt(Symbol symbol) 
+        => radicand = symbol;
+
+    public Sqrt(double num) 
+        => radicand = num;
+
+    #endregion
+
+    public override string ToString()
+        => $"Sqrt({radicand})";
+
+}
