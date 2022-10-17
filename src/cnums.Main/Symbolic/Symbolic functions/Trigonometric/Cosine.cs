203ca359a102ea928e898e3aa9d93512f8ec8429
Haydara <haydaraal2005@gmail.com>
2022-10-16 21:18:14 -0700

    Added sine and cosine classes for symbols

---
+++ src/cnums.Main/Symbolic/Symbolic functions/Trigonometric/Cosine.cs
@@ -0,0 +1,14 @@
+﻿namespace cnums.Symbolic;
+
+internal class Cosine : TrigonometricFunction
+{
+    #region Constructors
+
+    public Cosine(Function function)
+        : base(function) { type = "cos"; }
+
+    #endregion
+
+    public override string ToString()
+        => $"cos({Func})";
+}
---
+++ src/cnums.Main/Symbolic/Symbolic functions/Trigonometric/Sine.cs
@@ -0,0 +1,15 @@
+﻿namespace cnums.Symbolic;
+
+internal class Sine : TrigonometricFunction
+{
+
+    #region Constructors
+
+    public Sine(Function function)
+        : base(function) { type = "sin"; }
+
+    #endregion
+
+    public override string ToString()
+        => $"sin({Func})";
+}
