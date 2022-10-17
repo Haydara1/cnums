7224de19bb448824b984960ddf39721522be8b7c
Haydara <haydaraal2005@gmail.com>
2022-10-16 21:23:02 -0700

    Whitespace trailing

--- src/cnums.Main/cnumsException.cs
+++ src/cnums.Main/cnumsException.cs
@@ -5,19 +5,13 @@ public class CnumsException : Exception
     //Overriding the Message property
     public override string Message
     {
-        get
-        {
-            return "This exception was caused by a bug in the namespace, it is us, not you!";
-        }
+        get { return "This exception was caused by a bug in the namespace, it is us, not you!"; }
     }
 
     //Overriding the HelpLink Property
     public override string HelpLink
     {
-        get
-        {
-            return "It is probably marked as an issue: https://github.com/Haydara1/cnums/issues, otherwise, please let us know about it!";
-        }
+        get { return "It is probably marked as an issue: https://github.com/Haydara1/cnums/issues, otherwise, please let us know about it!"; }
     }
 
     public CnumsException() : base()
