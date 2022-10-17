bcdffb64681d0b27695fcd44bc2f7ae74ab62c2b
Haydara <haydaraal2005@gmail.com>
2022-10-16 21:18:59 -0700

    Added base class for all trigonometric functions classes

---
+++ src/cnums.Main/Symbolic/Symbolic functions/TrigonometricFunction.cs
@@ -0,0 +1,46 @@
+﻿namespace cnums.Symbolic;
+
+internal class TrigonometricFunction
+{
+    #region Properties
+
+	private Function function;	
+
+	public Function Func
+	{
+		get { return function; }
+		set { function = value; }
+	}
+
+	public string type = "";
+
+	#endregion
+
+	#region Constructors
+
+	public TrigonometricFunction(Function function)
+	{
+		this.function = function;
+	}
+
+	#endregion
+
+	public Function ToFunction()
+		=> new(this);
+
+    #region Addition
+
+	public static Function operator +(TrigonometricFunction trigonometric, double number)
+		=> new Polynomial(new() { new(trigonometric), new(number)}).ToFunction();
+
+	public static Function operator +(double number, TrigonometricFunction trigonometric)
+		=> trigonometric + number;
+
+	public static Function operator +(TrigonometricFunction trigonometric, Symbol symbol)
+		=> new Polynomial(new() { new(trigonometric), new(symbol) }).ToFunction();
+
+	public static Function operator +(Symbol symbol, TrigonometricFunction trigonometric)
+		=> trigonometric + symbol;
+
+    #endregion
+}
